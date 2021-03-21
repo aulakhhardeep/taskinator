6615d5a35092720f3c556ea38c3ce6bfb798c38e
Hardeep <aulakh.hardeep91@gmail.com>
2021-03-21 13:37:59 -0400

    Added functionality to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,29 @@ var tasksToDoEI = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event)
 {   event.preventDefault();
-
-    var listItemEI = document.createElement("li");
-    listItemEI.className = "task-item";
-    listItemEI.textContent = "This is a new task";
-    tasksToDoEI.appendChild(listItemEI);
     
-}
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // create list item
+    var listItemEl = document.createElement("li");
+    // give it a class name
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEI.appendChild(listItemEl);
+ 
+    
+};
 
 // name of button element is buttonEI, camelCase marks the element as JavaScript
 // EI identify it as a DOM element
