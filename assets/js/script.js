bee34c9e08071a08ea91b88bde72f805e3a3e004
Hardeep <aulakh.hardeep91@gmail.com>
2021-03-20 23:02:34 -0400

    Added form event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
-var buttonEI = document.querySelector("#save-task"); 
+var formEI = document.querySelector("#task-form"); 
 var tasksToDoEI = document.querySelector("#tasks-to-do");
-var createTaskHandler = function()
-{
+
+var createTaskHandler = function(event)
+{   event.preventDefault();
+
     var listItemEI = document.createElement("li");
     listItemEI.className = "task-item";
     listItemEI.textContent = "This is a new task";
     tasksToDoEI.appendChild(listItemEI);
+    
 }
 
 // name of button element is buttonEI, camelCase marks the element as JavaScript
 // EI identify it as a DOM element
-buttonEI.addEventListener("click",createTaskHandler); 
+formEI.addEventListener("submit",createTaskHandler); 
